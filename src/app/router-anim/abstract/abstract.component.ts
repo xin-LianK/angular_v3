304b6ba5b07f1d0417650a2fd5f921b2bd130e16
xin-LianK <350824110@qq.com>
2018-10-29 15:11:33 +0800

    动画回调

--- src/app/router-anim/abstract/abstract.component.ts
+++ src/app/router-anim/abstract/abstract.component.ts
@@ -35,6 +35,31 @@ export class AbstractComponent implements OnInit {
 
   ngOnInit() {
   }
+  startAnim(event: AnimationEvent) {
+    console.log(1);
+  }
+  endAnim(event: AnimationEvent) {
+    console.log(2);
+  }
+  onAnimationEvent(event: AnimationEvent) {
+    // openClose is trigger name in this example
+    // console.warn(`Animation Trigger: ${event.triggerName}`);
+
+    // // phaseName is start or done
+    // console.warn(`Phase: ${event.phaseName}`);
+
+    // // in our example, totalTime is 1000 or 1 second
+    // console.warn(`Total time: ${event.totalTime}`);
+
+    // // in our example, fromState is either open or closed
+    // console.warn(`From: ${event.fromState}`);
+
+    // // in our example, toState either open or closed
+    // console.warn(`To: ${event.toState}`);
+
+    // // the HTML element itself, the button in this case
+    // console.warn(`Element: ${event.element}`);
+  }
   toggle() {
     this.isOpen = !this.isOpen;
   }
